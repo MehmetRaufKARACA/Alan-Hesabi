2a2a7535402e76160b499cda8736f0a21b5e6cf9
Mehmet Rauf KARACA <mrk61rauf@gmail.com>
2022-01-01 14:57:11 +0300

    Yeniden düzenlendi

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main {
       // Değişkenleri tanımla
       double kenar1, kenar2, kenar3, alanxalan , u; 
       
-      // Scanner sınıfını tanımla
-      Scanner inp = new Scanner(System.in);
+      try (// Scanner sınıfını tanımla
+      Scanner inp = new Scanner(System.in)) {
+        // Kulanıcıdan dik kenar uzunluklarını al
+        System.out.print("1.kenar uzunluğunu giriniz : ");
+        kenar1 = inp.nextInt();
+        System.out.print("2.kenar uzunluğunu giriniz : ");
+        kenar2 = inp.nextInt();
+        System.out.print("3.kenar uzunluğunu giriniz : ");
+        kenar3 = inp.nextInt();
+      }
 
-      // Kulanıcıdan dik kenar uzunluklarını al
-      System.out.print("1.kenar uzunluğunu giriniz : ");
-      kenar1 = inp.nextInt();
-      System.out.print("2.kenar uzunluğunu giriniz : ");
-      kenar2 = inp.nextInt();
-      System.out.print("3.kenar uzunluğunu giriniz : ");
-      kenar3 = inp.nextInt();
-      
       // Üçgenin alanını bul ve ekrana yazdır
       u = (kenar1 + kenar2 + kenar3)/2;
       alanxalan = u * (u-kenar1) * (u-kenar2) * (u-kenar3);
